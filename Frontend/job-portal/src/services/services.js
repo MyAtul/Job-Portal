8df0ba1955171a18e4937c094aa13b64056410be
MyAtul <atuly2409@gmail.com>
2026-06-16 20:02:10 +0530

    Connect frontend to deployed backend

--- Frontend/job-portal/src/services/services.js
+++ Frontend/job-portal/src/services/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import App from '../App'
 
-const API_URL = "http://localhost:8080"
+const API_URL = "https://job-portal-backend-2913.onrender.com"
 
 const api = axios.create({
     baseURL:API_URL
